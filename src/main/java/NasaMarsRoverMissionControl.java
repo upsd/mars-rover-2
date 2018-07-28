40878f03a96763ff8e08f1138a5373c69d6330a3
upsd <up648421@myport.ac.uk>
2018-07-28 14:02:07 +0100

    Utilise injected parser.

--- src/main/java/NasaMarsRoverMissionControl.java
+++ src/main/java/NasaMarsRoverMissionControl.java
@@ -3,7 +3,7 @@ public class NasaMarsRoverMissionControl {
     private final Grid grid;
 
     public NasaMarsRoverMissionControl(String input, InputParser parser) {
-        grid = new InputParser().parseGridFrom(input);
+        grid = parser.parseGridFrom(input);
     }
 
     public String getFinalCoordinatesAndHeadings() {
