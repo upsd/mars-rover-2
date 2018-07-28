4586d07e5582ea570ab3d9d904df1efe7c5ed45a
upsd <up648421@myport.ac.uk>
2018-07-28 15:17:41 +0100

    Turn left command test passing.

--- src/main/java/LeftCommand.java
+++ src/main/java/LeftCommand.java
@@ -2,6 +2,9 @@ public class LeftCommand implements Command {
 
     @Override
     public Rover execute(Rover rover) {
+        if (rover.heading() == Heading.NORTH) {
+            return new Rover(new Point(rover.x(), rover.y()), Heading.WEST);
+        }
         return rover;
     }
 }
--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,13 +1,22 @@
 public class Rover {
 
+    private Point point;
     private Heading heading;
 
     public Rover(Point point, Heading heading) {
-
+        this.point = point;
         this.heading = heading;
     }
 
     public Heading heading() {
         return this.heading;
+    }
+
+    public int x() {
+        return this.point.x();
+    }
+
+    public int y() {
+        return this.point.y();
     }
 }
