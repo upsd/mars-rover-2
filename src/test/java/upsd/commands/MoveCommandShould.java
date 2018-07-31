e45b8f22a889fbda225eae524af2681ae365a23e
upsd <up648421@myport.ac.uk>
2018-07-31 11:00:51 +0100

    Clarify what MoveCommand unit tests are doing.

--- src/test/java/upsd/commands/MoveCommandShould.java
+++ src/test/java/upsd/commands/MoveCommandShould.java
@@ -1,122 +1,29 @@
 package upsd.commands;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import upsd.domain.Grid;
 import upsd.domain.Point;
 import upsd.domain.Rover;
-import upsd.headings.HeadingEast;
 import upsd.headings.HeadingNorth;
-import upsd.headings.HeadingSouth;
-import upsd.headings.HeadingWest;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
 public class MoveCommandShould {
 
-    @ParameterizedTest
-    @CsvSource({
-            "1, 2",
-            "2, 3",
-            "3, 4",
-            "4, 5"
-    })
-    void move_rover_within_bounds_of_grid_when_facing_north(int startingY, int expectedY) {
-        Rover rover = new Rover(new Point(1, startingY), new HeadingNorth());
-        MoveCommand command = new MoveCommand(new Grid(new Point(5, 5)));
-
-        Rover newRover = command.execute(rover);
-
-        assertThat(newRover.y(), is(expectedY));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "1, 2",
-            "2, 3",
-            "3, 4",
-            "4, 5"
-    })
-    void move_rover_within_bounds_of_grid_when_facing_east(int startingX, int expectedX) {
-        Rover rover = new Rover(new Point(startingX, 2), new HeadingEast());
-        MoveCommand command = new MoveCommand(new Grid(new Point(5, 5)));
-
-        Rover newRover = command.execute(rover);
-
-        assertThat(newRover.x(), is(expectedX));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "2, 1",
-            "3, 2",
-            "4, 3",
-            "5, 4"
-    })
-    void move_rover_within_bounds_of_grid_when_facing_south(int startingY, int expectedY) {
-        Rover rover = new Rover(new Point(2, startingY), new HeadingSouth());
-        MoveCommand command = new MoveCommand(new Grid(new Point(5, 5)));
-
-        Rover newRover = command.execute(rover);
-
-        assertThat(newRover.y(), is(expectedY));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "2, 1",
-            "3, 2",
-            "4, 3",
-            "5, 4"
-    })
-    void move_rover_within_bounds_of_grid_when_facing_west(int startingX, int expectedX) {
-        Rover rover = new Rover(new Point(startingX, 2), new HeadingWest());
-        MoveCommand command = new MoveCommand(new Grid(new Point(5, 5)));
-
-        Rover newRover = command.execute(rover);
-
-        assertThat(newRover.x(), is(expectedX));
-    }
-
     @Test
-    void move_rover_to_min_Y_of_grid_when_moving_northwards() {
-        Rover rover = new Rover(new Point(2, 2), new HeadingNorth());
-        MoveCommand command = new MoveCommand(new Grid(new Point(2, 2)));
+    void move_rover() {
+        HeadingNorth heading = mock(HeadingNorth.class);
+        Rover rover = new Rover(new Point(1, 2), heading);
+        Grid grid = mock(Grid.class);
+        MoveCommand command = new MoveCommand(grid);
 
-        Rover newRover = command.execute(rover);
+        when(heading.move(new Point(1, 2))).thenReturn(new Point(1, 4));
 
-        assertThat(newRover.y(), is(1));
-    }
 
-    @Test
-    void move_rover_to_min_x_of_grid_when_moving_eastwards() {
-        Rover rover = new Rover(new Point(2, 2), new HeadingEast());
-        MoveCommand command = new MoveCommand(new Grid(new Point(2, 2)));
+        command.execute(rover);
 
-        Rover newRover = command.execute(rover);
 
-        assertThat(newRover.x(), is(1));
-    }
-
-    @Test
-    void move_rover_to_max_y_of_grid_when_moving_southwards() {
-        Rover rover = new Rover(new Point(2, 1), new HeadingSouth());
-        MoveCommand command = new MoveCommand(new Grid(new Point(2, 2)));
-
-        Rover newRover = command.execute(rover);
-
-        assertThat(newRover.y(), is(2));
-    }
-
-    @Test
-    void move_rover_to_max_x_of_grid_when_moving_westwards() {
-        Rover rover = new Rover(new Point(1, 2), new HeadingWest());
-        MoveCommand command = new MoveCommand(new Grid(new Point(2, 2)));
-
-        Rover newRover = command.execute(rover);
-
-        assertThat(newRover.x(), is(2));
+        verify(heading).move(new Point(1, 2));
+        verify(grid).sanitisePoint(new Point(1, 4));
     }
 }
